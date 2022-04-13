dc7de697d0db9db338ce5524a63797c63dd4ae36
ThisIsAlperen <aydin.m.alperen@gmail.com>
2022-04-13 22:51:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // Application js code
+const StorageController = (function(){
+
+})();
+const UIController = (function(){
+
+})();
+const ProductController = (function(){
+
+})();
+const AppController = (function(Storage,UI,Product){
+
+})(StorageController,UIController,ProductController);
