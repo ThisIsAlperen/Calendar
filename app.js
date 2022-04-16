12604affaea9f0320fd3e4fde3018ddf0112eae5
ThisIsAlperen <aydin.m.alperen@gmail.com>
2022-04-16 21:57:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -23,11 +23,28 @@ const ProductController = (function () {
 })();
 const AppController = (function (Storage, UI, Product) {
     const loadEventListeners = function(){
-       
+       // Change page
+
+       document.querySelector('#nav-list').addEventListener('click', changePage)
+    }
+    const changePage = function(e){
+        e.preventDefault()
+        
+        if(e.target.innerText ==='Homepage'){
+            console.log('homepage')
+        }
+        if(e.target.innerText ==='Calendar'){
+            console.log('homepage')
+        }
+        if(e.target.innerText ==='Resume'){
+            console.log('Resume')
+        }
     }
     return {
         init: function(){
-
+            loadEventListeners()
         }
     }
 })(StorageController, UIController, ProductController);
+
+AppController.init()
